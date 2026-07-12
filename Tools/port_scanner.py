d651c9a7e1afa7e9496af1bcd3f2f2df1dc73fd2
mrt0bh <mjalmusali@gmail.com>
2026-07-12 07:31:41 +0300

    Simple port scanner with service detection and banner grabbing

--- Tools/port_scanner.py
+++ Tools/port_scanner.py
@@ -1,6 +1,5 @@
 import socket as sc
 
-
 def scan_port(ip, port):
     try:
         sock = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
@@ -25,18 +24,57 @@ def get_service(port):
         return "Unknown"
     
 
-def scan_ports(ip, port):
+def scan_ports(ip, ports):
     for port in ports:
         service = get_service(port)
 
         if scan_port(ip, port):
             print(f"[+] port {port} OPEN ({service})")
+
+            banner = grab_banner(ip, port)
+
+            if banner:
+                print("     Banner: ")
+                print(f"    {banner}")
         else:
             print(f"[-] port {port} CLOSED ({service})")
 
+def grab_banner(ip, port):
+    try:
+        sock = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
+        sock.settimeout(3)
+
+        sock.connect((ip, port))
+
+        #HTTP
+        if port in [80, 8080, 8000]:
+            request = f"GET / HTTP/1.1\r\nHOST: {ip}\r\nconnection: close\r\n\r\n"
+            sock.send(request.encode())
+
+        #FTP BANNER
+        elif port in [21, 22]:
+            pass
+        
+        #General
+        else:
+            sock.send(b"\r\n")
+
+        banner = sock.recv(4096)
+
+        sock.close()
+
+        response = banner.decode(errors="ignore")
+
+        if port in [80,8080,8000]:
+            return response.split("\r\n\r\n")[0]
+             
+        return response.strip()
+    
+    except Exception:
+        return None
 
 
-target = input("Enter your Target IP adrress: ")
+target = input("Enter your Target IP address: ")
 start_port = int(input("Enter Start Port: "))
 end_port = int(input("Enter end port: "))
 
